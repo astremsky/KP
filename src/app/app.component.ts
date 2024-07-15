be53e926354bac961acfbc12581fba94f3154d9a
Anton Astremskyi <Anton.Astremskyi@abcsys.com>
2024-07-15 10:51:45 -0400

    About us and Logo components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,21 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { LogoboxComponent } from './logobox/logobox.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    HeaderComponent, 
+    FooterComponent, 
+    MainComponent,
+    LogoboxComponent,
+    AboutComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/logobox/logobox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logobox',
+  standalone: true,
+  imports: [],
+  templateUrl: './logobox.component.html',
+  styleUrl: './logobox.component.scss'
+})
+export class LogoboxComponent {
+
+}

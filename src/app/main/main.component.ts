68ffaee967d8d0d7e97c7e35b2d044d3c13f6916
TargoniyAnatoliy <targoniyanatoliy@gmail.com>
2024-09-25 17:03:24 +0300

    add local copy autohelp

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,37 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+    selector: 'app-main',
+    standalone: true,
+    imports: [],
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.scss'
 })
 export class MainComponent {
 
 }
-
-//Main Cards animation
-document.addEventListener("DOMContentLoaded", function() {
-    
-  const cards = document.querySelectorAll('.card-custom');
-
-  const observerOptions = {
-      threshold: 0.3 // Відсоток появи картки у вікні перегляду (10%)
-  };
-
-  const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-          if (entry.isIntersecting) {
-              entry.target.classList.add('active');
-              // stop watching for card
-              observer.unobserve(entry.target);
-          }
-      });
-  }, observerOptions);
-
-  // start watching for every card
-  cards.forEach(card => {
-      observer.observe(card);
-  });
-});
